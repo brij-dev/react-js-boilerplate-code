d36580d9d3f09bbeffd21f6fb914f70d0ddf9f02
Brij <brijesh@rocketmedia.in>
2020-03-27 04:26:32 +0530

    app config added

---
+++ src/common/config.js
@@ -0,0 +1,20 @@
+// Info: Contains App specific Constants
+'use strict';
+
+
+export default {
+
+	BASE_URL                         :           'https://api.myjson.com/bins/',
+	API_ENDPOINT_SAMPLE              :           'arqmo',
+
+
+  GET_METHOD                       :           'GET',
+  POST_METHOD                      :           'POST',
+  JSON_REQUEST                     :           'json',
+
+  SERVER_ERROR: {
+    CODE: 'SERVER_ERROR',
+    MESSAGE: 'Internal Server Error'
+  }
+
+}
