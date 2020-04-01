0736dd0f3ebe5102e851f09f8ef0fde4e577087a
Brij <brijesh@rocketmedia.in>
2020-04-01 15:19:57 +0530

    enclosed app export inside withRouter

--- src/app.js
+++ src/app.js
@@ -74,4 +74,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(translate('translation')(App)));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(translate('translation')(App)));
